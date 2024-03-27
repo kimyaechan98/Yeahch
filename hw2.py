1ba98d25672eb4926941e6e75c3d479f35c6daf9
Yeahch <kimyaechan98@gmail.com>
2024-03-27 14:13:26 +0900

    Add Python File[hw2.py]

---
+++ hw2.py
@@ -0,0 +1,11 @@
+def get_integer(prompt) :
+    res = int(input(prompt))
+    return res;
+
+def set_all_bits(n) :
+    return(1 << n) - 1
+
+n = get_integer('설정할 비트 수는? ')
+b = set_all_bits(n)
+
+print(n, '비트를 모두 1로 설정한 수는' , b, '(', bin(b), ') 이다.')
