87483309230260776e2ddce91fa1186397d991d5
Yeahch <kimyaechan98@gmail.com>
2024-04-16 02:23:45 +0900

    Add File [hw1.py]

--- hw1.py
+++ hw1.py
@@ -1,15 +1,14 @@
 import math
 
-def get_radius(prompt):
+def get_radius(prompt) :
     r = int(input(prompt))
     return r
 
-def get_circle_area(radius):
-    area = 3.14 * radius ** 2
-    msg = f"{area}는 원의 넓이입니다."
-    return msg
-
-input_radius = get_radius('넓이를 구하고자 하는 원의 반지름은? ')
-result = get_circle_area(input_radius)
-print(result)
-print('원의 넓이 측정이 완료되었습니다.')
+def get_circle_area(반지름) :
+    원의_넓이 = (3.14*반지름**2)
+    return 원의_넓이
+    
+    
+원의반지름 =get_radius('넓이를 구하고자 하는 원의 반지름은?')
+result = get_circle_area(원의반지름)
+print('반지름', 원의반지름, '인 원의 넓이 = 3.14 x ', 원의반지름, 'X', 원의반지름, '=' , result)
