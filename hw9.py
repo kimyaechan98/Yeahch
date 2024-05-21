99706c0ce0af2f8fa198d70efc0aedd45a1d650e
kimyaechan98 <163804899+kimyaechan98@users.noreply.github.com>
2024-05-21 09:49:09 +0900

    Add files via upload

---
+++ hw9.py
@@ -0,0 +1,43 @@
+class Point:
+    def __init__(self, x=0, y=0):
+        self.__x = x
+        self.__y = y
+
+    def show(self):
+        print(f'({self.__x}, {self.__y})')
+
+    def set(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def get(self):
+        return (self.__x, self.__y)
+
+class Rectangle:
+    def __init__(self, x1, y1, x2, y2):
+        self.lt = Point(x1, y1)
+        self.rb = Point(x2, y2)
+
+    def show(self):
+        lt_coords = self.lt.get()
+        rb_coords = self.rb.get()
+        print(f'좌측 상단 꼭지점이 {lt_coords}이고 우측 하단 꼭지점이 {rb_coords}인 사각형입니다.')
+
+    def getWidth(self):
+        return self.rb.get()[0] - self.lt.get()[0]
+
+    def getHeight(self):
+        return self.rb.get()[1] - self.lt.get()[1]
+
+    def getArea(self):
+        return self.getWidth() * self.getHeight()
+
+    def getPerimeter(self):
+        return 2 * (self.getWidth() + self.getHeight())
+
+
+r1 = Rectangle(5, 5, 20, 10)
+a = r1.getArea()
+p = r1.getPerimeter()
+r1.show()
+print(f'\n넓이는 {a}, 둘레는 {p}')
